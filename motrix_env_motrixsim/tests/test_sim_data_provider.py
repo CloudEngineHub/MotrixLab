2c796cd468bcc5ac88167a38fd89347d87954071
zilch <147668916@qq.com>
2026-09-15 10:49:43 +0800

    feat: per-body compile-time BodyModel on SimModel (#46)

--- motrix_env_motrixsim/tests/test_sim_data_provider.py
+++ motrix_env_motrixsim/tests/test_sim_data_provider.py
@@ -356,10 +356,10 @@ class _FrameworkTestBackend(SimBackend):
         raise AssertionError("not exercised")
 
     @property
-    def model_query_compiler(self):
+    def model_compiler(self):
         return self
 
-    def compile(self, queries: Mapping[str, ModelQuery]) -> object:
+    def compile(self, scene: object, queries: Mapping[str, ModelQuery]) -> object:
         raise AssertionError("not exercised")
 
     @property
